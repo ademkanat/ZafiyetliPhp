e2b65cd8f3b20bf786d04c803d9291cbf6caa200
ademkanat <suatademkanat@gmail.com>
2019-11-02 14:24:42 +0300

    Create passwordByPass.php

---
+++ passwordByPass.php
@@ -0,0 +1,16 @@
+public Response checkLogin(Session session) {
+try {
+String uname = session.getParameter(“username”);
+String passwd = session.getParameter(“password”);
+User user = db.getUser(uname, passwd);
+if (user == null) {
+// invalid credentials
+session.setMessage(“Login failed. “);
+return doLogin(session);
+}
+}
+catch (Exception e) {}
+// valid user
+session.setMessage(“Login successful. “);
+return doMainMenu(session);
+}
